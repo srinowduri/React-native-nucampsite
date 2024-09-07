94f9fe529af532ae8e72cc293e5cb53bd42d6cd4
Srilaxmi Nowduri <nowduribtv@gmail.com>
2024-09-07 11:31:52 -0500

    set up redux, activity indicator, Redux Adding Favorites

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,35 +1,44 @@
-import RenderCampsite from '../features/campsites/RenderCampsite';
-import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { COMMENTS } from '../shared/comments';
+import { useSelector, useDispatch } from 'react-redux';
+import RenderCampsite from '../features/campsites/RenderCampsite';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    const [comments, setComments] = useState(COMMENTS);
-    const [favorite, setFavorite] = useState(false);
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
                 <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
-                <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
             </View>
         );
-    }
+    };
+
     return (
         <FlatList
-            data={comments.filter(
+            data={comments.commentsArray.filter(
                 (comment) => comment.campsiteId === campsite.id
             )}
             renderItem={renderCommentItem}
             keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20 }}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite campsite={campsite} 
-                                    isFavorite={favorite}
-                                    markFavorite={() => setFavorite(true)}/>
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                    />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
             }
